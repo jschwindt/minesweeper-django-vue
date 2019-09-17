0ab329eb163ab5133ba2deaef8aca809b12067b7
Juan Schwindt <juan@schwindt.org>
2019-09-16 21:23:41 -0300

    Add basic player (user) signup and signin

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,4 +1,4 @@
-"""main URL Configuration
+"""minesweeper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('players/', include('players.urls')),
+    path('players/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
